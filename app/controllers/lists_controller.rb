1751a76efc3d2187e82320fc19bd975c8678f397
Jared Clemmensen and Russell Hofvendahl <russell@>
2018-04-16 16:12:55 -0700

    more changes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,6 +17,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
+      flash[:notice] = "List successfully added!"
       redirect_to lists_path
     else
       render :new
